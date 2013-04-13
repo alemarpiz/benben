288664fe8d7e5eda8d9a532d41323f8cb8fbddbd
David Glick <david@glicksoftware.com>
2013-04-13 15:28:33 -0700

    add basic page model

--- benben/__init__.py
+++ benben/__init__.py
@@ -1,20 +1,21 @@
 from pyramid.config import Configurator
+from pyramid.threadlocal import get_current_registry
 from sqlalchemy import engine_from_config
-
-from .models import (
-    DBSession,
-    Base,
-    )
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    from benben.models import initialize_sql
     engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
+    initialize_sql(engine, drop_all=True)
+
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
+
+
+def get_settings():
+    return get_current_registry().settings
--- benben/models.py
+++ benben/models.py
@@ -1,28 +1,122 @@
+from pyramid.traversal import resource_path
 from sqlalchemy import (
     Column,
+    ForeignKey,
     Integer,
-    Text,
+    MetaData,
+    String,
+    Unicode,
+    UniqueConstraint,
     )
-
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.ext.orderinglist import ordering_list
 from sqlalchemy.orm import (
+    backref,
+    relation,
     scoped_session,
     sessionmaker,
     )
-
 from zope.sqlalchemy import ZopeTransactionExtension
+import os
+import transaction
 
+metadata = MetaData()
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
+Base.metadata = metadata
+Base.objects = DBSession.query_property()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+class Page(Base):
+    """A basic page in the content hierarchy."""
 
-    def __init__(self, name, value):
+    __tablename__ = 'pages'
+    __table_args__ = (
+        UniqueConstraint('parent_id', 'name'),
+        )
+    __mapper_args__ = dict(
+        polymorphic_on='type',
+        polymorphic_identity='page',
+        with_polymorphic='*',
+        )
+
+    #: Primary key for the node in the DB
+    #: (:class:`sqlalchemy.types.Integer`)
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    #: Lowercase class name of the node instance
+    #: (:class:`sqlalchemy.types.String`)
+    type = Column(String(30), nullable=False)
+    #: ID of the node's parent
+    #: (:class:`sqlalchemy.types.Integer`)
+    parent_id = Column(ForeignKey('pages.id'))
+    #: Position of the node within its container / parent
+    #: (:class:`sqlalchemy.types.Integer`)
+    position = Column(Integer())
+    #: Name of the node as used in the URL
+    #: (:class:`sqlalchemy.types.Unicode`)
+    name = Column(Unicode(50), nullable=False)
+    #: Title of the node, e.g. as shown in search results
+    #: (:class:`sqlalchemy.types.Unicode`)
+    title = Column(Unicode(100))
+
+    _children = relation(
+        'Page',
+        collection_class=ordering_list('position'),
+        order_by=[position],
+        backref=backref('parent', remote_side=[id]),
+        cascade='all',
+        )
+
+    def __init__(self, name=None, parent=None, title=u""):
+        """Constructor"""
         self.name = name
-        self.value = value
+        self.parent = parent
+        self.title = title
+
+    @property
+    def __name__(self):
+        return self.name
+
+    @property
+    def __parent__(self):
+        return self.parent
+
+    @__parent__.setter
+    def __parent__(self, value):
+        self.parent = value
+
+    def __repr__(self):
+        return '<%s %s at %s>' % (
+            self.__class__.__name__, self.id, resource_path(self))
+
+    def __eq__(self, other):
+        return isinstance(other, Page) and self.id == other.id
+
+    def __ne__(self, other):
+        return not self == other
+
+
+def populate():
+    """
+    Create the root page (:class:`~benben.models.Page`)
+    if there are no pages in the tree yet.
+    """
+
+    if Page.objects.count() == 0:
+        DBSession.add(Page(name=u'root', title=u'Benben'))
+
+
+def initialize_sql(engine, drop_all=False):
+    DBSession.registry.clear()
+    DBSession.configure(bind=engine)
+    metadata.bind = engine
+
+    if drop_all or os.environ.get('BENBEN_TEST_DB_STRING'):
+        metadata.reflect()
+        metadata.drop_all(engine)
+
+    metadata.create_all(engine)
+    populate()
+    transaction.commit()
+
+    return DBSession
